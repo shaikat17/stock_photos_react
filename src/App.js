3686eaf7f378ba20f624424f71a4d435e06fff0e
shaikat17 <shaikatpal56@gmail.com>
2023-02-05 11:34:39 +0600

    working on api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+
+import Photo from "./Photo";
+
+const mainUrl = `https://api.unsplash.com/photos/`;
+const searchUrl = `https://api.unsplash.com/search/photos/`;
 
 function App() {
+  const [loading, setLoading] = useState(false)
+  const [photos, setPhotos] = useState([])
+
+  const fetchImages = async () => {
+    let url;
+    url = `${mainUrl}?client_id=sScaMD6EiQINBRNWxbaoprVTHSOL26PWIfx38G-obbE`
+
+    try {
+      setLoading(true)
+      const response = await fetch(url)
+      const data = await response.json()
+      console.log(data);
+    } catch (error) {
+      setLoading(false)
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchImages()
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Stock Photos</h1>
+    </>
   );
 }
 
